26c823e06c3286ff46819c7952d87db0d32d6314
Kajal Pandey <pandey.kajal919@gmail.com>
2025-04-12 14:42:36 +0530

    Unit Test to verify number of api calls in FakeStoreProductServiceTest.java

--- src/test/java/com/scaler/projectservicejan31capstone/Services/FakeStoreProductServiceTest.java
+++ src/test/java/com/scaler/projectservicejan31capstone/Services/FakeStoreProductServiceTest.java
@@ -11,7 +11,9 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class FakeStoreProductServiceTest {
@@ -23,7 +25,7 @@ public class FakeStoreProductServiceTest {
     public void testGetProductByIdReturnsProduct() throws ProductNotFoundException {
         FakeStoreProductsDTO dummyResponse = new FakeStoreProductsDTO();
         dummyResponse.setId(1L);
-        dummyResponse.setTitle("Title");
+        dummyResponse.setTitle("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops");
         dummyResponse.setDescription("description");
         dummyResponse.setPrice(22);
         dummyResponse.setImage("img.url");
@@ -49,5 +51,29 @@ public class FakeStoreProductServiceTest {
 
         assertThrows(ProductNotFoundException.class, () ->
                 fakeStoreProductService.getProductById(1L));
+    }
+    @Test
+    public void testCreateProductVerifyAPICalls(){
+        FakeStoreProductsDTO dummyResponse = new FakeStoreProductsDTO();
+        dummyResponse.setId(1L);
+        dummyResponse.setTitle("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops");
+        dummyResponse.setDescription("description");
+        dummyResponse.setPrice(22);
+        dummyResponse.setImage("img.url");
+        dummyResponse.setCategory("category");
+
+
+        when(restTemplate.postForObject(
+                eq("https://fakestoreapi.com/products"),
+                any(),
+                eq(FakeStoreProductsDTO.class))).thenReturn(dummyResponse);
+
+            Product product = fakeStoreProductService.createProduct("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", "description", 22, "img.url",
+                     "category");
+            verify(restTemplate, times(1)).postForObject(
+                    eq("https://fakestoreapi.com/products"),
+                    any(),
+                    eq(FakeStoreProductsDTO.class));
+
     }
 }
